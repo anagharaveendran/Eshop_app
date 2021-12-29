5f9eff151e8113816818fcb89784c1ea0ffc717f
anaghar <ranagha40@gmail.com>
2021-12-29 21:17:37 +0530

    Placing Order Page

---
+++ Frontend/src/components/admin/Admin.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+
+
+
+
+export default function Admin() {
+    
+
+  return (<div>
+    
+
+  </div>);
+}
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import Navigation from './common/Navigation';
 import Product from './components/product/Product';
 import ProductDetails from './components/product/ProductDetails';
+import Order from './components/order/Order';
+import Admin from './components/admin/Admin';
 //import Home from './components/home/Home';
 
 
@@ -12,8 +14,10 @@ import ProductDetails from './components/product/ProductDetails';
 //   document.getElementById('root')
 // );
 
+
 ReactDOM.render(
-  <BrowserRouter><ProductDetails/></BrowserRouter>,
-    document.getElementById('root')
-  );
+<BrowserRouter><Admin/></BrowserRouter>,
+  document.getElementById('root')
+);
+
 
